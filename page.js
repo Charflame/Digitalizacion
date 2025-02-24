c9d97091e549b5cdbc8b5317a3cb68ad5b47b54e
Charflame <carlosjeperez2@outlook.es>
2025-02-24 10:58:09 +0100

    upload 1

--- page.js
+++ page.js
@@ -1,22 +1,14 @@
+productos = ["Minecraft", "Doom", "Half-Life", "Portal", "Elden Ring", "Starcraft: Brood War", "E.T", "Factorium"]
+imagenes = []
+precios = []
+
 let boton = document.getElementById("boton");
 let numero_cuadros = 0;
 let contenedor = document.getElementById("contenedor")
+let numero_productos = 0    
 
-function sumar_cuadro(){
-    let nuevoDiv = document.createElement("div");
-    numero_cuadros = numero_cuadros+1;
-    nuevoDiv.textContent = "cuadro nº " + numero_cuadros;
-    let nuevoImg = document.createElement("img");
-    nuevoImg.src= "https://i.pinimg.com/236x/91/2c/65/912c6562d1c47e6d386139624af4b717.jpg"
-    nuevoDiv.appendChild(nuevoImg)
-    nuevoDiv.classList.add("cuadro")
-    nuevoImg.classList.add("imagen")
+function borrar_productos(){
+    }
 
-    contenedor.appendChild(nuevoDiv);
-}
-
-for(let contador = 0; contador < 16; contador = contador + 1){
-    sumar_cuadro()
-}
 
 boton.addEventListener("click", sumar_cuadro)
