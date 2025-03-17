58d15cc1d819f109aa29b65eadb1bb334b826c68
Charflame <carlosjeperez2@outlook.es>
2025-03-17 10:53:17 +0100

    upload 5

--- page.js
+++ page.js
@@ -2,7 +2,7 @@ productos = ["Minecraft", "Doom", "Half-Life", "Portal", "Elden Ring", "Starcraf
 imagenes = ["https://assetsio.gnwcdn.com/co49x5.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp",
 "https://s1.elespanol.com/2017/07/20/actualidad/actualidad_232741836_129966103_1706x1280.jpg",
 "https://static.wikia.nocookie.net/halflife7283/images/e/ec/Half-Lifeboxart.jpg/revision/latest?cb=20131211041722&path-prefix=es",
-"https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2022/09/portal_rtx_portada-scaled.jpg?fit=2560%2C1440&quality=50&strip=all&ssl=1",
+"https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Portal_standalonebox.jpg/220px-Portal_standalonebox.jpg",
 "https://preview.redd.it/bueqtztxmnj81.png?auto=webp&s=692ee8026d707e035977daf27c5ef6298e019718",
 "https://i.3djuegos.com/juegos/2658/starcraft_brood_war/fotos/ficha/starcraft_brood_war-1686850.jpg",
 "https://upload.wikimedia.org/wikipedia/en/thumb/f/f8/Etvideogamecover.jpg/220px-Etvideogamecover.jpg",
@@ -12,7 +12,9 @@ precios = ["30€", "10€","8€", "10€", "60€", "15€", "40€", "32€"]
 let boton = document.getElementById("boton");
 let numero_cuadros = 0;
 let contenedor = document.getElementById("contenedor")
-let numero_productos = 0    
+let numero_productos = 0  
+let lista = document.getElementById("lista")  
+let borrador = document.getElementById("eraser")
 
 function anadir_productos(){
 
@@ -21,26 +23,34 @@ function anadir_productos(){
     let nuevoImg = document.createElement("img");
 
     
-    nuevoDivtexto.textContent = productos[numero_cuadros] + " " + precios[numero_cuadros];
-    nuevoImg.src= imagenes[numero_cuadros]
+
+    nuevoDivtexto.textContent = productos[numero_cuadros] + " " + precios[numero_cuadros]; 
+    nuevoImg.src = imagenes[numero_cuadros]
     nuevoDiv.appendChild(nuevoImg)
     nuevoDiv.appendChild(nuevoDivtexto)
     
-
     nuevoDiv.classList.add("cuadro")
     nuevoImg.classList.add("imagen")
 
+    nuevoDiv.addEventListener("click", comprar_productos.bind(null,numero_cuadros))
+
     contenedor.appendChild(nuevoDiv);
     numero_cuadros = numero_cuadros+1;
+
+}
+
+function comprar_productos(numero){
+    let nuevaCompra = document.createElement("div");
+    nuevaCompra.textContent= productos[numero] + " " + precios[numero]
+
+    lista.appendChild(nuevaCompra)
 }
 
 for(let contador = 0; contador < 8; contador = contador + 1){
     anadir_productos()
 }
 
-function comprar_productos(){
-    let nuevaCompra = document.createElement("div");
-}
+
 
 function borrar_productos(){
 
